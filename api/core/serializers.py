06f4c5303fa562a0bade185e4699cdf73d81eabe
Adam Brown <ajbrow12@coastal.edu>
2024-02-15 23:09:21 -0500

    Added Flight Serializer

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -14,3 +14,10 @@ class RunwaySerializer(serializers.ModelSerializer):
         model = Runways
         fields = ['aiport', 'runway_number', 'runway_designation', 'length', 'width']
         read_only_fields = ['id']
+
+class FlightSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Flights
+        fields = ['origin', 'destination', 'airline', 'departure', 'arrival', 'flight_number', 'aircraft_type']
+        read_only_fields = ['id']
