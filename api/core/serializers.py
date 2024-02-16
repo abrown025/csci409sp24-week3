8570c4015020f4ff4959906a5e0e5eee9c062852
Adam Brown <ajbrow12@coastal.edu>
2024-02-15 23:01:02 -0500

    Made Database Modifications

---
+++ api/core/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from core.models import Airline, Airport, Runways, Flights
+
+class AirlineSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Airline
+        fields = ['airport', 'runway_number', 'length', 'width']
+        read_only_fields = ['id']
